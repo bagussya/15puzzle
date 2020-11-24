25bc8a26cef80617421e5796d0d5c8ac71562f9e
38bal <74639787+38bal@users.noreply.github.com>
2020-11-24 21:31:55 +0700

    Create MainFrame.java

---
+++ src/MainFrame.java
@@ -0,0 +1,22 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame {
+    
+    public MainFrame(){
+        JFrame frame = new JFrame();    //window
+        
+        JPanel panel = new JPanel();    //layout di windownya
+        panel.setBorder(BorderFactory.createEmptyBorder(300,300,300,300));  //border layout
+        panel.setLayout(new GridLayout());  //layout
+        
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("15 Puzzle");    //judul window
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
