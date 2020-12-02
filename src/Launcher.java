ee9bf2ffaa0cda65ee376fd0a52504e09f30fd24
bagussya <74695094+bagussya@users.noreply.github.com>
2020-12-03 01:32:14 +0700

    Add files via upload

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,8 +1,22 @@
+package test;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Launcher {
-    
-    public static void main(String[] args) {
-        MainFrame mf = new MainFrame();
-        mf.setVisible(true);
-    }
+
+	public static void main(final String[] args) {
+	    SwingUtilities.invokeLater(() -> {
+	        final JFrame frame = new JFrame();
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setTitle("Game of Fifteen");
+	        frame.setResizable(false);
+	        frame.add(new Main(4, 550, 30), BorderLayout.CENTER);
+	      	frame.pack();
+	      	frame.setLocationRelativeTo(null);
+	      	frame.setVisible(true);
+	    });
+	}   
 }
